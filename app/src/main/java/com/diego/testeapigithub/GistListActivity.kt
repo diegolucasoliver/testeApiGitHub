8ed473b6544de7114961fc1b3ed1ad038235ce26
Diego Medeiros <diegolucasoliver@hotmail.com>
2019-08-10 14:00:38 -0300

    Renomeando a activity

--- app/src/main/java/com/diego/testeapigithub/GistListActivity.kt
+++ app/src/main/java/com/diego/testeapigithub/GistListActivity.kt
@@ -1,12 +1,12 @@
 package com.diego.testeapigithub
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class GistListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_gist_list)
     }
 }
